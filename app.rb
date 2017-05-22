0ca5a6095b1d1c3f9a190b6474657fab305837e1
Panteha <panteha.ahmadi@gmail.com>
2017-05-22 17:08:56 +0100

    added index.erb, updated app.rb

--- app.rb
+++ app.rb
@@ -1,21 +1,6 @@
 require 'sinatra'
 require 'shotgun'
 
-get '/' do
-  "Hi Panteha!"
-end
-
-get '/secret' do
-  "Very quitetly now."
-end
-
 get '/cat' do
-   "<div class='border'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>
-   <style contenteditable>
-    .border { border: 3px dashed red;
-           padding: 5;
-           }
-   </style>"
+  erb(:index)
 end
