185bf615db354c2ad4ebe9aa8903b80c14fb7b86
RSijelmass <R.Sijelmass@gmail.com>
2017-05-23 12:14:03 +0100

    retrieving from params and added @mood

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 require 'shotgun'
 
-get '/cat' do
+get '/random-cat' do
   @name = %w(Amigo Oscar Viking).sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @mood = params[:mood]
   erb(:index)
 end
