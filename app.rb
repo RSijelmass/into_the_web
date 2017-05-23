a1f0a489059f0589616e37518ab429ffdd51edfc
RSijelmass <R.Sijelmass@gmail.com>
2017-05-23 18:30:58 +0100

    Removed commentary text

--- app.rb
+++ app.rb
@@ -6,21 +6,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
-  p params
-  @name = params[:name]
-  @mood = params[:mood]
-  erb(:index)
-end
-
 post '/named-cat' do
-  p params
   @name = params[:name]
   @mood = params[:mood]
   erb(:index)
 end
 
 get '/form' do
-p params
-erb(:form)
+  erb(:form)
 end
