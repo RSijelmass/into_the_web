6cc10525619dd7e869d481ec52d44cdbe6ae177f
toby <toby.hw@live.co.uk>
2017-05-23 13:57:37 +0100

    Adds Post method in form.erb Adds POST instead of get in app.rb method

--- app.rb
+++ app.rb
@@ -12,3 +12,15 @@ get '/named-cat' do
   @mood = params[:mood]
   erb(:index)
 end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  @mood = params[:mood]
+  erb(:index)
+end
+
+get '/form' do
+p params
+erb(:form)
+end
